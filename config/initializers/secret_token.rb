4a7c0e09af0fca5913f8c13d1c8b6cc8c47d4459
RickWayne <fewayne@wisc.edu>
2013-11-13 17:17:26 -0600

    Initial. Hope I got database.yml excluded this time.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoilsAgWx::Application.config.secret_key_base = 'd917d9ebf7885e14628aa790addb235e358b7b4da537085b0d38d6d760d65f9134927a1ed0e4860525276ac20c926bebfc4729ee143d3e53fbd504c194d542bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SoilsAgWx::Application.config.session_store :cookie_store, key: '_soils_ag_wx_session'
