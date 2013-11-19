0819c8223b6fb71341400f3a93e10b8496a1e4eb
RickWayne <fewayne@wisc.edu>
2013-11-19 17:52:16 -0600

    Buncha other hacks to get pages up or apologize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def borked
+    str = <<-END
+      <h3>Page under reconstruction</h3>
+      <p>Due to a major overhaul of Soil Science's Web infrastucture, some of our products are broken right now. Unfortunately, you've found one. We are working hard on it and hope to have them all back up in a couple of days. We apologize for the inconvenience. (2013-11-19)</p>
+    END
 end
