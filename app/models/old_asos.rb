263a1c78f48fb91a1617596f0e8c3738a774a931
RickWayne <fewayne@wisc.edu>
2013-11-21 14:38:29 -0600

    Think this has got it.

--- app/models/old_asos.rb
+++ app/models/old_asos.rb
@@ -6,16 +6,22 @@ class OldAsos < ActiveRecord::Base
     database: 'asos'
   self.table_name = "asosData"
   
+  def self.time_for(date,time)
+    Time.utc(date.year,date.month,date.day,time.hour,time.min,time.sec)
+  end
+  
   def self.translate
-    start_year = first.date.year
+    return unless (d = first["Date"])
+    start_year = d.year
     stns = AsosStation.all.inject({}) { |hash, station| hash.merge(station.stnid => station[:id]) }
     (start_year..2013).each do |year|
       print "\n#{year}: "
       for stnid in stns.keys
         print " #{stnid}"
         where('ID = ? and Date >= ? and Date <= ?',stnid,Date.civil(year,1,1),Date.civil(year,12,31)).each do |old|
+          date = old["Date"]
           AsosDatum.create!(
-            date: old["Date"], nominal_time: old["NominalTime"], actual_time: old["ActualTime"], asos_station_id: stns[stnid],
+            date: date, nominal_time: time_for(date,old["NominalTime"]), actual_time: time_for(date,old["ActualTime"]), asos_station_id: stns[stnid],
             t_dew: old["TDew"], t_air: old["TAir"], windspeed: old["WindSpeed"], pressure: old["Pressure"], precip: old["Precip"],
             wind_dir:old["WindDir"]
             )
