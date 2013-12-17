67c47f0a35390e46de7533332fd2f095765b4c82
RickWayne <fewayne@wisc.edu>
2013-12-17 16:54:13 -0600

    Added helper, fixed alfalfa, added corn dev

--- app/helpers/thermal_models_helper.rb
+++ app/helpers/thermal_models_helper.rb
@@ -1,2 +1,14 @@
 module ThermalModelsHelper
+  def ddgif_image_link(image_file,alt="Degree-Day Map")
+    url =<<-END
+    <a href="http://agwx.soils.wisc.edu/soils-agwx-assets/uwex_agwx/images/ddgifs/#{image_file}">
+      <IMG SRC="http://agwx.soils.wisc.edu/soils-agwx-assets/uwex_agwx/images/ddgifs/#{image_file}" ALT="#{alt}" height="400" width="480">
+      END
+    url.html_safe
+  end
+  
+  def ddgif_url(link_text,image_file)
+    link_to link_text, "http://agwx.soils.wisc.edu/soils-agwx-assets/uwex_agwx/images/ddgifs/#{image_file}"
+  end
+  
 end
