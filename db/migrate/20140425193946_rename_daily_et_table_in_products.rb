cb413653320eb3735d0597797716f08e8ce4ca45
Rick Wayne <fewayne@wisc.edu>
2014-04-25 14:44:25 -0500

    Except it helps if you get the table name right.
    Dork.

--- db/migrate/20140425193946_rename_daily_et_table_in_products.rb
+++ db/migrate/20140425193946_rename_daily_et_table_in_products.rb
@@ -1,5 +1,5 @@
 class RenameDailyEtTableInProducts < ActiveRecord::Migration
   def change
-    Product.where(data_table_name: 'WiMnDET').each { |product| product.data_table_name = 'wi_mn_d_ets'; product.save! }
+    Product.where(data_table_name: 'WiMnDET').each { |product| product.data_table_name = 'wi_mn_dets'; product.save! }
   end
 end
