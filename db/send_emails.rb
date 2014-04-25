19e64ff2e9f5683efc9ab9428f02647c415c5357
Rick Wayne <fewayne@wisc.edu>
2014-04-25 13:42:01 -0500

    OK, I think this is ready for production.

--- db/send_emails.rb
+++ db/send_emails.rb
@@ -1,29 +1,26 @@
 require File.join(File.dirname(__FILE__),'..','config','environment.rb')
 
 def send_emails(params)
-  if params[:id] == '0'
-    raise "not ready to send to everyone yet!"
-    subscribers = Subscriber.all
+  if params[:id] == 0
+    puts "sending to everyone"
+    subscribers = Subscriber.where("email like ?",'fewayne%')
   else
     subscribers = [Subscriber.find(params[:id])]
   end
-  puts "all selected subscribers " +subscribers.inspect
   subscribers = subscribers.select { |subs| subs.has_confirmed }
-  puts "confirmed subscribers now "+subscribers.inspect
-  start_date = finish_date = 1.day.ago
+  start_date = end_date = 1.day.ago
   begin
     start_date = Date.parse(params[:start_date]) if params[:start_date]
-    finish_date = Date.parse(params[:finish_date]) if params[:finish_date]
+    end_date = Date.parse(params[:end_date]) if params[:end_date]
   rescue Exception => e
     start_date = 1.day.ago unless start_date
-    finish_date = 1.day.ago unless finish_date
+    end_date = 1.day.ago unless end_date
   end
   
   sent = 0
   results = []
   subscribers.each do |subs|
-    email = SubscriptionMailer.product_report(subs,start_date,finish_date)
-    puts email.body.encoded
+    email = SubscriptionMailer.product_report(subs,start_date,end_date)
     unless email.body.encoded
       puts "No body"
       next
@@ -40,13 +37,34 @@ def send_emails(params)
       puts "No ET value"
       next
     end
-    puts "Sent:\n" + email.body.encoded
     results << email.deliver
     sent += 1
   end
-  results.each { |res| puts res.inspect }
   puts "Sent #{sent} emails"
 end
 
 rick = Subscriber.find_by_email('fewayne@wisc.edu')
-send_emails({id: rick[:id]})
+if ARGV[0]
+  if ARGV[0] == 'everybody'
+    id = 0
+  else
+    id =ARGV[0].to_i
+  end
+else
+  puts "usage: send_emails.rb {everybody|id} [start_date] [end_date] (a test id for rick is #{rick[:id]})"
+  exit
+end
+
+if ARGV[1]
+  start_date = Date.parse(ARGV[1])
+else
+  start_date = nil
+end
+
+if ARGV[2]
+  end_date = Date.parse(ARGV[2])
+else
+  end_date = nil
+end
+
+send_emails({id: id, start_date: start_date, end_date: end_date})
--- test/mailers/subscription_mailer_test.rb
+++ test/mailers/subscription_mailer_test.rb
@@ -7,6 +7,9 @@ class SubscriptionMailerTest < ActionMailer::TestCase
   LATITUDE = 42.0
   LONGITUDE = -89.0
   ETS = [0.05,0.10,0.15,0.20,0.25,0.30,0.35]
+  AGO_ET = 0.9
+  AGO_REGEXP = /,0\.9<br\/>/
+  DAYS_AGO = 10
   
   def setup
     @etgrid = Product.create! name: 'ET', data_table_name: 'wi_mn_dets', type: 'GridProduct', subscribable: true 
@@ -14,6 +17,7 @@ class SubscriptionMailerTest < ActionMailer::TestCase
     @rick_subs = Subscription.create! latitude: LATITUDE, longitude: LONGITUDE,
       product_id: GridProduct.first[:id], doy_start: 1, doy_end: 365
     ETS.each_with_index { |et,ii| WiMnDet.create! date: days_back(ii), latitude: LATITUDE, w892: et }
+    WiMnDet.create! date: days_back(DAYS_AGO), latitude: LATITUDE, w892: AGO_ET
     @rick.subscriptions << @rick_subs; @rick.save!
   end
   
@@ -41,9 +45,17 @@ class SubscriptionMailerTest < ActionMailer::TestCase
       date = days_back(ii).to_s
       assert(results =~ /#{date},#{et}/,"Could not find a line with #{date} and #{et}\n#{WiMnDet.all.collect {|et| et.date }.inspect}")
     end
-    
   end
 
+  test "long product report" do
+    results = SubscriptionMailer.product_report(@rick,days_back(DAYS_AGO),days_back(0)).body.encoded
+    assert(results =~ Regexp.new(days_back(DAYS_AGO).to_s),"results: \n#{results}\n\nshould have contained '#{days_back(DAYS_AGO)}'")
+    assert(results =~ Regexp.new(AGO_ET.to_s),"results: \n#{results}\n\nshould have contained '#{AGO_ET}'")
+    results = SubscriptionMailer.product_report(@rick,days_back(2),days_back(0)).body.encoded
+    assert(results !~ Regexp.new(days_back(DAYS_AGO).to_s),"results: \n#{results}\n\nshould not have contained '#{days_back(DAYS_AGO)}'")
+    assert(results !~ AGO_REGEXP,"results: \n#{results}\n\nshould not have contained '#{AGO_ET}'")
+  end
+  
   test "special" do
     mail = SubscriptionMailer.special(@rick,'')
     assert_equal "Special", mail.subject
