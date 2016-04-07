293f009ac8fdd9cb74d87794e78af2fb733e379f
Allen Ballman <aballman@gmail.com>
2016-04-06 20:49:43 -0400

    Changed the base url for ag-weather to be configurable

--- app/models/wi_mn_det.rb
+++ app/models/wi_mn_det.rb
@@ -8,8 +8,9 @@ class WiMnDet < ActiveRecord::Base
 
   def self.daily_series(start_date, end_date, long, lat)
     long *= -1 if long < 0
-    url = "http://localhost:8080/evapotranspirations?lat=#{lat}&long=#{long}&start_date=#{start_date}&end_date=#{end_date}"
-    response = HTTParty.get(url)
+    base_url = ENV['AG_WEATHER_BASE_URL'] || 'http://localhost:3000'
+    url = "#{base_url}/evapotranspirations?lat=#{lat}&long=#{long}&start_date=#{start_date}&end_date=#{end_date}"
+    response = HTTParty.get(url, { timeout: 5 })
     body = JSON.parse(response.body)
     body.map { |h| [h['date'], h['value']] }.to_h
   end
